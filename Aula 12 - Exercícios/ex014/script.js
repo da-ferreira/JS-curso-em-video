b328e5fd3bcbbc35b9a27aa812aaa23559efef71
da-ferreira <ferreiradavid361@gmail.com>
2022-04-22 10:42:48 -0300

    Aula 12 - ex014

--- Aula 12 - Exercícios/ex014/script.js
+++ Aula 12 - Exercícios/ex014/script.js
@@ -1,2 +1,21 @@
 
-alert("Olá")
+function carregar() {
+    var mensagem = document.getElementById('mensagem')
+    var imagem = document.getElementById('imagem')
+    var horas = (new Date()).getHours()
+
+    // var horas = 19
+
+    mensagem.innerText = `Agora são ${horas} horas.`
+
+    if (horas >= 0 && horas < 12) {
+        imagem.src = "imagens/fotomanha.png"
+        document.body.style.background = "#e2cd9f"
+    } else if (horas <= 18) {
+        imagem.src = "imagens/fototarde.png"
+        document.body.style.background = "#b9846f"
+    } else {
+        imagem.src = "imagens/fotonoite.png"
+        document.body.style.background = "#515154"
+    }
+}
